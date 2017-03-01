60dde9a65538b0b70f928fea6ceeeafde45d7329
Fernando Toledo <fernando.toledo@gmail.com>
2017-03-01 01:16:21 -0300

    refac connection manager

--- src/main/java/com/dev/web/mobile/dao/HerokuConnectionManager.java
+++ src/main/java/com/dev/web/mobile/dao/HerokuConnectionManager.java
@@ -3,6 +3,7 @@ package com.dev.web.mobile.dao;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -11,25 +12,20 @@ public class HerokuConnectionManager implements ConnectionManager {
 
 	private static ConnectionManager connectionManager = null;
 	private static BasicDataSource dataSource = null;
+	private String dbUrl;
 
 	@Override
 	public Connection getConnection() throws SQLException {
-		return dataSource.getConnection();
+		Connection connection = DriverManager.getConnection(this.dbUrl);
+		return connection;
 	}
 
 	HerokuConnectionManager() throws URISyntaxException {
-		URI uri = new URI(System.getenv("DATABASE_URL"));
-		String username = uri.getUserInfo().trim().split(":")[0];
-		String password = uri.getUserInfo().trim().split(":")[1];
-		;
-		String host = "jdbc:postgresql://" + uri.getHost().trim() + uri.getPath().trim();
-
-		dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl(host);
-		dataSource.setUsername(username);
-		dataSource.setPassword(password);
-		dataSource.setInitialSize(10);
+		URI uri = new URI(System.getenv("DATABASE_URL"));		
+		String dbUrl = System.getenv("JDBC_DATABASE_URL");
+	    String username = System.getenv("JDBC_DATABASE_USERNAME");
+	    String password = System.getenv("JDBC_DATABASE_PASSWORD");	    
+	    this.dbUrl = dbUrl;
 	}
 
 	public static synchronized ConnectionManager getInstance() throws URISyntaxException {
